445fbb4b95df89297021de9b5f5e5cdcedbbe047
Chayanan Pathumarak <Chayananpath.work@gmail.com>
2026-05-11 23:33:37 +0700

    bill-smmy#2

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,20 +1,41 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'core/router/app_router.dart';
+
+// void main() {
+//   runApp(
+//     const ProviderScope(
+//       child: MyApp(),
+//     ),
+//   );
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp.router(title: 'BillGang', routerConfig: appRouter);
+//   }
+// }
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'core/router/app_router.dart';
+import 'features/bill/bill_summary_screen.dart';
 
 void main() {
-  runApp(
-    const ProviderScope(
-      child: MyApp(),
-    ),
-  );
+  runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp.router(title: 'BillGang', routerConfig: appRouter);
+  Widget build(BuildContext context, WidgetRef ref) {
+    return const MaterialApp(
+      title: 'BillGang',
+      home: BillSummaryScreen(),
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
