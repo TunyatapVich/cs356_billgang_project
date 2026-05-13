4c24fb6b96c0e01e472b2b87994dbcd6da90c57d
Thep ;p <tunyatap.vich@bumail.net>
2026-05-13 08:10:54 +0700

    style: format payload generation for better readability

--- frontend/test_qr.dart
+++ frontend/test_qr.dart
@@ -3,6 +3,9 @@ import 'package:thaiqr/thaiqr.dart';
 
 void main() {
   final generator = ThaiQRGenerator();
-  final payload = generator.generateCodeFromMobileOrId('0843654067', 10.50.toString());
+  final payload = generator.generateCodeFromMobileOrId(
+    '0843654067',
+    10.50.toString(),
+  );
   print(payload);
 }
